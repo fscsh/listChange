64b9e5deabae1651100d9eab31b2152a3aafea10
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-10-03 08:48:28 -0700

    finish

--- js/listchange.js
+++ js/listchange.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $(".list-1").bind("click",function(){
+    $(".list-2").bind("click",function(){
         $(".list-1").css("backgroundPosition","0px -26px");
         $(".list-2").css("backgroundPosition","-30px -26px");
         $(".changelist").children().removeClass("list-1-0").addClass("list-2-0")
     })
-    $('.list-2').bind("click",function(){
+    $('.list-1').bind("click",function(){
         $(".list-1").css("backgroundPosition","0px 0px");
         $(".list-2").css("backgroundPosition","-30px 0px");
         $(".changelist").children().removeClass("list-2-0").addClass("list-1-0")
